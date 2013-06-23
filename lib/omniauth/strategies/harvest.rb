8b2ec47b0b11cc3ccd9ad21245eb7b598098c62d
Brendan Loudermilk <brendan@gophilosophie.com>
2013-06-23 14:47:25 -0700

    Updates and improves functionality
    * Bumps dependencies to latest
    * Implements the required OmniAuth schema
    * Removes default `OmniAuth::Strategies::OAuth2` behavior

--- lib/omniauth/strategies/harvest.rb
+++ lib/omniauth/strategies/harvest.rb
@@ -1,35 +1,36 @@
-require 'omniauth/strategies/oauth2'
+require "omniauth/strategies/oauth2"
 
 module OmniAuth
   module Strategies
     class Harvest < OmniAuth::Strategies::OAuth2
       option :client_options, {
-        :site => 'https://api.harvestapp.com',
-        :authorize_url => '/oauth2/authorize',
-        :token_url => '/oauth2/token'
+        :site => "https://api.harvestapp.com",
+        :authorize_url => "/oauth2/authorize",
+        :token_url => "/oauth2/token"
       }
-      
-      def authorize_params
-        super.tap do |params|
-          params[:response_type] = "code"
-          params[:client_id] = client.id
-          params[:redirect_uri] ||= callback_url
-        end
-      end
 
-      def request_phase
-        super
-      end
+      uid { raw_info["user"]["id"] }
 
-      def build_access_token
-        token_params = {
-          :code => request.params['code'],
-          :redirect_uri => callback_url,
-          :client_id => client.id,
-          :client_secret => client.secret,
-          :grant_type => 'authorization_code'
+      info do
+        {
+          :name => full_name,
+          :email => raw_info["user"]["email"],
+          :first_name => raw_info["user"]["first_name"],
+          :last_name => raw_info["user"]["last_name"],
+          :image => raw_info["user"]["avatar_url"]
         }
-        client.get_token(token_params)
+      end
+
+      extra do
+        { :raw_info => raw_info }
+      end
+
+      def raw_info
+        @raw_info ||= access_token.get("/account/who_am_i.json").parsed
+      end
+
+      def full_name
+        raw_info["user"]["first_name"] + " " + raw_info["user"]["last_name"]
       end
     end
   end
